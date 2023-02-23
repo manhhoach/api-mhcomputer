b4e9ce3c41790468e902063068674ced68a18026
manhhoach <manhhn.nv5@gmail.com>
2023-02-23 09:48:44 +0700

    add validations

--- src/validations/address.js
+++ src/validations/address.js
@@ -13,9 +13,11 @@ let detailAddress = Joi.object({
 })
 
 const addressSchema = Joi.object({
-    userId: Joi.number().integer().optional(),
     name: Joi.string().max(255).optional(),
-    phone: Joi.string().max(20).optional(),
+    phone: Joi.string().pattern(/((84|0)[3|5|7|8|9])+([0-9]{8})\b/).optional().messages({
+        'string.pattern.base': '{{#label}} with value {:[.]} fails to match the required pattern'
+       // 'string.pattern.base': '{{#label}} with value {:[.]} fails to match the required pattern: {{#regex}}'
+    }),
     typeAddress: Joi.number().valid(0, 1).optional(),
     detailAddress: detailAddress.optional(),
     street: Joi.string().max(512).optional(),
@@ -26,7 +28,7 @@ const addressSchema = Joi.object({
 const validateAddress = (req, res, next) => {
     const { error } = addressSchema.validate(req.body);
     if (error) {
-        console.log(error);
+        console.log(error.details);
         return next(new AppError(400, error.details[0].message))
     }
     next();
