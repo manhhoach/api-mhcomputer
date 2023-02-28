f48d7aeacc5a7cd608c0a6a3b3467774687b9c0a
manhhoach <manhhn.nv5@gmail.com>
2023-02-28 21:31:44 +0700

    nothing

--- src/app.js
+++ src/app.js
@@ -3,9 +3,10 @@ const app = express()
 require('dotenv').config();
 const route = require('./routes')
 const cors = require('cors')
-const helmet=require('helmet')
-const compression=require('compression')
-const {responseWithError}=require('./utils/response')
+const helmet = require('helmet')
+const compression = require('compression')
+const { responseWithError } = require('./utils/response')
+
 
 
 app.use(cors());
@@ -17,19 +18,20 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 
+
 app.get('/', (req, res) => {
-  let str="hello cụ mạnh"
-  res.send(str.repeat(90000))
-  //res.send('Welcome to MH-Computer!')
+  //let str="hello mạnh"
+  //res.send(str.repeat(90000))
+  res.send('Welcome to MH-Computer!')
 })
 
 app.use(route)
 
-app.use((err, req, res, next)=>{
-  console.log('err in app: ',err);
+app.use((err, req, res, next) => {
+  console.log('err in app: ', err);
   res.status(err.statusCode || 500).json(responseWithError(err))
 })
 
 
 
-module.exports=app
+module.exports = app
