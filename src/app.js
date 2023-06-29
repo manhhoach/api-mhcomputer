c167b5f014380d510c2a2aef685efba2308f187b
manhhoach <manhhn.nv5@gmail.com>
2023-06-29 21:38:17 +0700

    add swagger

--- src/app.js
+++ src/app.js
@@ -6,8 +6,11 @@ const cors = require('cors')
 const helmet = require('helmet')
 const compression = require('compression')
 const { responseWithError } = require('./utils/response')
-const path=require('path')
+const path = require('path')
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs');
 
+const swaggerDocument = YAML.load(path.join(__dirname, 'public/api.yaml'));
 
 app.use(cors());
 app.use(compression({
@@ -19,12 +22,11 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
-})
+
+
+app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use(route)
-
 app.use((err, req, res, next) => {
   console.log('err in app: ', err);
   res.status(err.statusCode || 500).json(responseWithError(err))
